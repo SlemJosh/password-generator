1c8e7c8716d94fedfc897da0a0cfb087a6ac4579
SlemJosh <joshua.slem@gmail.com>
2023-09-07 21:19:51 -0600

    Thanks to this evenings class, found a way to simply my variables

--- assets/script.js
+++ assets/script.js
@@ -18,10 +18,14 @@
 
 // Lets test some variables.
 var lowerCase = ['a', 'b','c','d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var lowerCase2 = ['abcdefghijklmnopqrstuvwxyz'.split('')]; //picked this up in Thursdays class.  
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numeric = ['0','1','2','3','4','5','6','7','8','9'];
 var specialChar =['`','~','!','@','#','$','%','^','&','*'];  //Finish these
 console.log(upperCase.length);
+
+console.log(lowerCase2);
+console.log(lowerCase2.length);
 
 console.log(lowerCase);
 console.log(lowerCase.length);
